10835b53f7fe36f1314e36b3a2d01560baeb044b
chedli01 <chedli.masmoudi01@gmail.com>
2025-10-17 17:37:46 +0100

    create signals for user and token and computed for isAuthenticated + new login and logout logic

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, inject, signal } from '@angular/core';
+import { Injectable, computed, inject, signal } from '@angular/core';
 import { CredentialsDto } from '../dto/credentials.dto';
 import { LoginResponseDto } from '../dto/login-response.dto';
 import { HttpClient } from '@angular/common/http';
 import { API } from '../../../config/api.config';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { ConnectedUser } from '../user/user';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +13,12 @@ export class AuthService {
   private http = inject(HttpClient);
 
 //////////////////////////////////////////////////////////////
+user = signal<ConnectedUser|null>(null)
+token = signal<string|null>(null)
+
+isAuthenticated = computed(()=>!!this.token)
+
+
   
 
 
@@ -35,20 +42,45 @@ export class AuthService {
   /** Inserted by Angular inject() migration for backwards compatibility */
   constructor(...args: unknown[]);
 
-  constructor() {}
+  constructor() {
+    const savedToken = localStorage.getItem('token');
+    const savedUserId = Number(localStorage.getItem('userId'));
+    const savedUserEmail = localStorage.getItem('userEmail');
+
+    if (savedToken) {
+      this.token.set(savedToken);
+      this.user.set(savedUserId && savedUserEmail ? {id:savedUserId,email:savedUserEmail} : null);
+    }
+  }
 
   login(credentials: CredentialsDto): Observable<LoginResponseDto> {
     return this.http.post<LoginResponseDto>(API.login, credentials);
   }
-  loginTest(credentials:CredentialsDto):string{
-    return "hi"
-  }
+  ///////////////////////////////////////////////////
+  loginTest(credentials:CredentialsDto): Observable<LoginResponseDto>{
+     return this.http.post<LoginResponseDto>(API.login, credentials).pipe(
+      tap((response) => {
+        this.token.set(response.id);
+        this.user.set({email:credentials.email,id:response.userId})
 
-  isAuthenticated(): boolean {
-    return !!localStorage.getItem('token');
+        localStorage.setItem('token', response.id);
+        localStorage.setItem('userId', response.userId.toString());
+        localStorage.setItem('userEmail', credentials.email);
+      })
+    );
+    
   }
+//////////////////////////////////////////////////////////////////////////
+  // isAuthenticated(): boolean {
+  //   return !!localStorage.getItem('token');
+  // }
 
   logout() {
     localStorage.removeItem('token');
+  }
+  logoutTest() {
+    this.token.set(null);
+    this.user.set(null);
+    localStorage.clear();
   }
 }
